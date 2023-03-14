04d7f6049d1ca2d86d6aa359837745ef8ed117c2
Rubén Velásquez <73437475+ruben-velasquez@users.noreply.github.com>
2023-03-14 15:51:50 +0000

    Se le agregó el parámetro target  al comando Avatar

--- Commands/avatar.js
+++ Commands/avatar.js
@@ -3,9 +3,14 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 module.exports = {
   data: new SlashCommandBuilder()
   .setName('avatar')
-  .setDescription('Muestra tu avatar'),
+  .setDescription('Muestra tu avatar o el de otro usuario')
+  .addUserOption(option => 
+    option.setName('target')
+    .setDescription('El usuario del que quieres ver el avatar')
+    .setRequired(false)),
   async execute (interaction, client) {
-    let user = interaction.user;
+    let target = interaction.options.getUser('target');
+    let user = target || interaction.user;
     let avatarembed = new EmbedBuilder()
     .setImage(user.avatarURL())
     .setColor("#DF0000")
